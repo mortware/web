da86449832b2874d9485c1530a6e3ee819a1371e
David Price <david@mortware.net>
2020-10-25 11:45:02 +0000

    Update Home.js

--- src/views/Home.js
+++ src/views/Home.js
@@ -5,6 +5,7 @@ export default () => {
     return (
         <div className="container mx-auto">
             <h1 className="font-bold text-2x1 mb-3">
+                Wibbly
             </h1>
             <div className="md:flex flex-wrap md:mx-3">
             </div>

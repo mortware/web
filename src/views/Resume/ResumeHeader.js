3bdc421122afad245bff11ec0e748312a07df287
David Price <david@mortware.net>
2020-10-30 09:26:51 +0000

    Changes to home page and resume

--- src/views/Resume/ResumeHeader.js
+++ src/views/Resume/ResumeHeader.js
@@ -5,8 +5,8 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 export default () => {
 
     let links = Data.links.map((link, i) =>
-        <li className="mb-1 md:mb-3 print:mb-1" key={i}>
-            <a href={link.url} className="flex flex-row flex-end md:flex-row-reverse text-gray-600 md:text-gray-500 text-sm md:text-base whitespace-no-wrap truncate" target="_blank" rel="noopener noreferrer">
+        <li className="mb-1 md:mb-2 print:mb-1" key={i}>
+            <a href={link.url} className="flex flex-row items-center flex-end md:flex-row-reverse print:flex-row-reverse text-gray-600 md:text-gray-500 text-sm md:text-base whitespace-no-wrap truncate" target="_blank" rel="noopener noreferrer">
                 <FontAwesomeIcon icon={link.icon} fixedWidth size="lg" className="mx-1 md:mx-2" />
                 <span className="inline-block print:inline-block">{link.display}</span>
             </a>
@@ -35,7 +35,7 @@ export default () => {
                     </div>
 
                     {/* Link */}
-                    <div className="p-2 md:p-4 lg:p-4 print:p-0 w-full md:w-auto">
+                    <div className="p-2 print:p-0 w-full md:w-auto">
                         <ul className="list-unstyled">
                             {links}
                         </ul>
--- src/views/Resume/index.js
+++ src/views/Resume/index.js
@@ -1,10 +1,52 @@
 import React from 'react';
-import Skills from './Skills';
 import ResumeHeader from './ResumeHeader';
 import * as Data from './ResumeData';
+import Tags from '../../components/Tags';
 import './Styles.scss';
-import EmploymentHistoryItem from './EmploymentHistoryItem';
-import ResumeSection from './ResumeSection';
+
+const History = ({ key, title, employer, description, from, to, notes, tags }) => {
+    return (
+        <article key={key} className="resume-timeline-item mb-4 md:mb-8 last:mb-0 relative">
+
+            <header className="mb-1 md:mb-2">
+                <div className="flex justify-between ">
+                    <h3 className="font-bold whitespace-no-wrap truncate">{title}</h3>
+                    <div className="text-sm font-medium whitespace-no-wrap truncate">{employer}</div>
+                </div>
+                <div className="text-sm text-gray-500">{from} - {to}</div>
+            </header>
+
+            <p className="mb-1 md:mb-4">
+                {description}
+            </p>
+
+            <div className="mb-4">
+                {/* <h4 className="font-bold mb-1">Achievements:</h4> */}
+                <ul className="list-disc list-outside pl-0 print:text-sm">
+                    {notes.map((note) =>
+                        <li className="ml-5">
+                            {note}
+                        </li>
+                    )}
+                </ul>
+            </div>
+            <div>
+                {/* <h4 className="font-bold mb-1">Technologies used:</h4> */}
+                <Tags tags={tags} theme="light" />
+            </div>
+        </article>
+    )
+}
+
+const Section = ({ title, children }) => {
+
+    return (
+        <section className="resume-section mb-4">
+            <h2 className="font-bold pb-3 mb-6 uppercase text-xl print:pb-1 print:mb-1">{title}</h2>
+            {children}
+        </section>
+    )
+}
 
 const Resume = () => {
 
@@ -15,30 +57,30 @@ const Resume = () => {
 
             <section className="p-4 md:p-12 print:p-0 print:pt-2">
 
-                <ResumeSection title="Career Summary">
+                <Section title="Career Summary">
                     <p className="mb-0">
                         {Data.summary.map((line) => (
                             <p className="mb-2">{line}</p>
                         ))}
                     </p>
-                </ResumeSection>
+                </Section>
 
-                <ResumeSection title="Skills & Tools">
+                <Section title="Skills & Tools">
                     {Data.skills.map(category =>
                         <p>
                             <strong>{category.name}: </strong>
                             {category.skills.sort((a, b) => a > b ? 1 : -1).join(", ")}
                         </p>
                     )}
-                </ResumeSection>
+                </Section>
 
-                <ResumeSection title="Work Experience">
+                <Section title="Work Experience">
                     <div className="resume-timeline pl-8">
                         {Data.workHistory
                             .sort((a, b) => a.to < b.to ? 1 : -1)
                             .slice(0, 5)
                             .map((item, i) =>
-                                <EmploymentHistoryItem
+                                <History
                                     key={i}
                                     title={item.title}
                                     employer={item.employer}
@@ -50,10 +92,10 @@ const Resume = () => {
                                 />
                             )}
                     </div>
-                </ResumeSection>
+                </Section>
 
-                <ResumeSection title="Education">
-                    <div className="grid grid-cols-1 md:grid-cols-3 gap-2">
+                <Section title="Education">
+                    <div className="grid grid-cols-1 md:grid-cols-3 print:grid-cols-3 gap-2">
                         {Data.education.map((item, i) =>
                             <div key={i} className="mb-2 flex flex-no-wrap space-x-1 md:flex-col md:space-x-0 print:flex-col print:space-x-0">
                                 <div className="font-bold whitespace-no-wrap truncate">{item.name}</div>
@@ -62,7 +104,7 @@ const Resume = () => {
                             </div>
                         )}
                     </div>
-                </ResumeSection>
+                </Section>
 
             </section>
         </article >

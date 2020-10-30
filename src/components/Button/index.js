51bc2633f32abd1bb6560d8c1ce8e76dd7d0a64a
David Price <david@mortware.net>
2020-10-30 16:00:13 +0000

    Fixed links

--- src/components/Button/index.js
+++ src/components/Button/index.js
@@ -5,7 +5,7 @@ import "./styles.scss";
 
 const Button = ({ url, label, icon, cta }) => {
     return (
-        <a href={url} target="_blank" className={`btn ${cta && "btn-cta"}`}>
+        <a href={url} target="_blank" className={`btn ${cta && "btn-cta"}`} rel="noopener noreferrer">
             {icon !== null &&
                 <FontAwesomeIcon icon={icon} size="lg" className="mx-1 md:mx-2" />
             }

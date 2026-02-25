f162c13b02b33e75aaef1e86515ad67a45b788f9
David Price <david.price2@property.nhs.uk>
2026-02-25 15:09:46 +0000

    Remove unused Link import from App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { Link, Route, Routes } from 'react-router-dom'
+import { Route, Routes } from 'react-router-dom'
 import { ThemeToggle } from './ThemeToggle'
 import ResumePage from './resume/ResumePage'
 
